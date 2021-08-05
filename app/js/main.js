ee1139adec9ff95a9e919ddf6355873cad08d40d
Omelaa <ostpa28@gmail.com>
2021-08-05 23:07:55 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+
+$(function(){
+  
+});
+let myImageSlider = new Swiper('.partners__items',{
+  slidesPerView: 5,
+  spaceBetween: 0,
+  loop: true,
+
+  autoplay: {
+		// Пауза между прокруткой
+		delay: 2000,
+		// Закончить на последнем слайде
+		stopOnLastSlide: false,
+		// Отключить после ручного переключения
+		disableOnInteraction: false
+	},
+  speed: 300,
+});
