cb5be1602bfd5610e21c1da8875db7536a79824a
Omelaa <ostpa28@gmail.com>
2021-08-06 21:23:30 +0300

    7 sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,12 @@
 
-$(function(){
   
-});
 let myImageSlider = new Swiper('.partners__items',{
   slidesPerView: 5,
   spaceBetween: 0,
   loop: true,
-
+  
   autoplay: {
-		// Пауза между прокруткой
+    // Пауза между прокруткой
 		delay: 2000,
 		// Закончить на последнем слайде
 		stopOnLastSlide: false,
@@ -16,4 +14,11 @@ let myImageSlider = new Swiper('.partners__items',{
 		disableOnInteraction: false
 	},
   speed: 300,
+});
+  
+  
+$(function(){
+
+       var mixer = mixitup('.prices__items');
+          
 });
